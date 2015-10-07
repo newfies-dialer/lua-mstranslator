c817317f53646007ba2a91817ef13b62137f11d7
areski <areski@gmail.com>
2015-10-07 13:56:30 +0200

    import src for mstranslator

--- README.rst
+++ README.rst
@@ -1,8 +1,8 @@
-===============================
+================
 Lua MSTranslator
-===============================
+================
 
-:Author: Areski Belaid
+:Author: Arezqui Belaid and Joshua Patten
 :Description: Microsoft Translator API Helper for Lua
 :Documentation: https://github.com/newfies-dialer/lua-mstranslator
 :Contributors: `list of contributors <https://github.com/newfies-dialer/lua-mstranslator/graphs/contributors>`_
@@ -12,7 +12,88 @@ Lua MSTranslator
         :target: https://travis-ci.org/newfies-dialer/lua-mstranslator
 
 
+
+Lua Microsoft Translate Wrapper
+-------------------------------
+
+lua-mstranslate is a library to produce a text-to-speech file using `Microsoft Translate`_ web services.
+
+In order to utilize this service you must sign up for Microsoft Translator service and register an application. More information on creating a Microsoft account is located at the `getting started with Microsoft Translator API`_ page.
+
+
+Quickstart
+----------
+
+::
+
+    MSTranslate = require "mstranslate"
+
+    CLIENT_ID = 'XXXXXXXXXXXX'
+    CLIENT_SECRET = 'YYYYYYYYYYYYYY'
+    SERVICE_URL = 'http://api.microsofttranslator.com/V2/Http.svc/Speak'
+
+    tts_mstranslate = MSTranslate:new(CLIENT_ID, CLIENT_SECRET, SERVICE_URL, directory)
+
+    TEXT = "This is a test of the Microsoft Translate text to speech service."
+    LANG = 'EN'
+    tts_mstranslate:prepare(TEXT, LANG)
+    output_filename = tts_mstranslate:run()
+
+    print("Recorded TTS = "..output_filename)
+
+
 Features
 --------
 
-* TODO
+* Produce text to speech in different languages
+
+Dependencies
+------------
+
+There are a few dependencies: md5, lfs and lua-curl.
+We use this version of lua curl : http://msva.github.com/lua-curl/
+
+To install md5 and lfs::
+
+    luarocks install md5
+    luarocks install luafilesystem
+
+
+To install lua-curl::
+
+    cd /usr/src/
+    wget https://github.com/msva/lua-curl/archive/master.zip -O lua-curl.zip
+    unzip lua-curl.zip
+    cd lua-curl-master
+    cmake . && make install
+
+
+Feedback
+--------
+
+Feedback are more than welcome, post bugs and feature requests on github:
+
+http://github.com/newfies-dialer/lua-mstranslate/issues
+
+
+Extra information
+-----------------
+
+Newfies-Dialer, an Open Source Voice BroadCasting Solution, uses this module to synthetize audio files being play to the end-user.
+Further information about Newfies-Dialer can be found at http://www.newfies-dialer.org
+
+This module is built and supported by Star2Billing : http://www.star2billing.com
+
+Similar library in Python : http://github.com/newfies-dialer/python-mstranslate
+
+
+Source download
+---------------
+
+The source code is currently available on github. Fork away!
+
+http://github.com/newfies-dialer/lua-mstranslate
+
+
+.. _Microsoft Translate: http://www.microsoft.com/en-us/translator/translatorapi.aspx
+.. _getting started with Microsoft Translator API: https://www.microsoft.com/en-us/translator/getstarted.aspx
--- src/mstranslator.lua
+++ src/mstranslator.lua
@@ -1,51 +1,206 @@
 --
--- mstranslator-0.1.0
+-- mstranslate.lua - Lua wrapper for text-to-speech synthesis with Microsoft Translate
+-- Copyright (C) 2015 Arezqui Belaid <areski@gmail.com> and Joshua Patten <joshpatten@gmail.com>
 --
--- The MIT License (MIT)
---
--- Copyright (c) 2015, Areski Belaid
---
--- Permission is hereby granted, free of charge, to any person obtaining a copy of
--- this software and associated documentation files (the "Software"), to deal in
--- the Software without restriction, including without limitation the rights to
--- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
--- the Software, and to permit persons to whom the Software is furnished to do so,
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation files
+-- (the "Software"), to deal in the Software without restriction,
+-- including without limitation the rights to use, copy, modify, merge,
+-- publish, distribute, sublicense, and/or sell copies of the Software,
+-- and to permit persons to whom the Software is furnished to do so,
 -- subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+-- BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+-- ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+-- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
 
--- The above copyright notice and this permission notice shall be included in all
--- copies or substantial portions of the Software.
+local md5 = require "md5"
+require "lfs"
+require "cURL"
 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
--- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
--- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
--- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
--- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Check file exists and readable
+function file_exists(path)
+    local attr = lfs.attributes(path)
+    if (attr ~= nil) then
+        return true
+    else
+        return false
+    end
+end
+
+--
+-- Get an url and save result to file
+--
+function ms_wget(url, auth_header, outputfile)
+    -- open output file
+    f = io.open(outputfile, "w")
+    local text = {}
+    local function writecallback(str)
+        f:write(str)
+        return string.len(str)
+    end
+    local c = cURL.easy_init()
+    -- setup url
+    c:setopt_url(url)
+    -- Set auth header
+    c:setopt_httpheader({auth_header})
+    -- perform, invokes callbacks
+    c:perform({writefunction = writecallback})
+    -- close output file
+    f:close()
+    return table.concat(text,'')
+end
+
+--
+-- URL Encoder
+--
+function url_encode(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w ])",
+        function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
+
+--
+-- Token Generator for MS Translate API
+--
+function ms_token_gen(clientID, clientSecret)
+        local json = require "json"
+        local authUrl = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/"
+        local params = {
+                scope = "http://api.microsofttranslator.com",
+                grant_type = "client_credentials",
+                client_id = clientID,
+                client_secret = clientSecret
+        }
+        local get_params = ''
+        -- URLEncode the parameters
+        for k, v in pairs(params) do
+            if get_params ~= '' then
+                get_params = get_params..'&'
+            end
+            get_params = get_params..tostring(k)..'='..url_encode(v)
+        end
+        local c = cURL.easy_init()
+        c:setopt_url(authUrl)
+        -- We have to post
+        c:setopt_post(1)
+        c:setopt_postfields(get_params)
+        -- Avoid any SSL heartburn
+        c:setopt_ssl_verifypeer(0)
+        -- Pipe the return response to a function that fills 'buffer' variable
+        c:perform({writefunction = function(str)
+                                         buffer = str
+                                   end})
+        -- Turn the JSON response string into a table
+        jdata = json.decode(buffer)
+        -- Return the access token field
+        return jdata["access_token"]
+end
+
+
+
+--
+-- MSTranslate Class
 --
 
-local PKG_AUTHOR = 'Areski Belaid'
-local PKG_EMAIL = 'areski@gmail.com'
-local PKG_VERSION = '0.1.0-1'
+local MSTranslate = {
+    -- default field values
+    CLIENT_ID = 'XXXXXXXXXXXX',
+    CLIENT_SECRET = 'YYYYYYYYYYYYYY',
 
+    SERVICE_URL = 'http://api.microsofttranslator.com/V2/Http.svc/Speak',
+    LANGUAGE = 'en',
+    DIRECTORY = '/tmp/',
 
-local mstranslator = {
-    _VERSION = '0.1.0-1',
+    -- Properties
+    filename = nil,
+    cache = true,
+    data = {},
+    langs = {},
 }
 
+-- Meta information
+MSTranslate._COPYRIGHT   = "Copyright (C) 2015 Areski and Joshua"
+MSTranslate._DESCRIPTION = "Lua wrapper for text-to-speech synthesis with Microsoft Translate"
+MSTranslate._VERSION     = "lua-mstranslate 0.1"
 
-function mstranslator:new (o)
+
+function MSTranslate:new (o)
     o = o or {}   -- create object if user does not provide one
     setmetatable(o, self)
     self.__index = self
     return o
 end
 
-function mstranslator:method(text)
-    -- docstring
-    if string.len(text) == 0 then
+function MSTranslate:prepare(textstr, lang)
+    -- Prepare Microsoft Translate TTS
+    if string.len(textstr) == 0 then
         return false
     end
-    return true
+    lang = string.lower(lang)
+    concatkey = textstr..'-'..lang
+    hash = md5.sumhexa(concatkey)
+
+    key = 'mstranslate'..'_'..hash
+    self.filename = key..'-'..lang..'.wav'
+
+    self.data = {
+        language = lang,
+        format = 'audio/wav',
+        options = 'MinSize',
+        text = textstr,
+    }
 end
 
-return mstranslator
+function MSTranslate:set_cache(value)
+    -- Enable Cache of file, if files already stored return this filename
+    self.cache = value
+end
+
+function MSTranslate:run()
+    -- Run will call Microsoft Translate API and reproduce audio
+
+    -- Check if file exists
+    if self.cache and file_exists(self.DIRECTORY..self.filename) then
+        return self.DIRECTORY..self.filename
+    else
+        -- Generate Authorization Token
+        token = ms_token_gen(self.CLIENT_ID, self.CLIENT_SECRET)
+        -- Build Authorization Header
+        auth_header = "Authorization: Bearer "..token
+        -- Get all the Get params and encode them
+        get_params = ''
+        for k, v in pairs(self.data) do
+            if get_params ~= '' then
+                get_params = get_params..'&'
+            end
+            get_params = get_params..tostring(k)..'='..url_encode(v)
+        end
+
+        -- print("===HTTP=== HEADER:"..auth_header.."\nCALL:"..self.SERVICE_URL..'?'..get_params, self.DIRECTORY..self.filename)
+        ms_wget(self.SERVICE_URL..'?'..get_params, auth_header, self.DIRECTORY..self.filename)
+
+        if file_exists(self.DIRECTORY..self.filename) then
+            return self.DIRECTORY..self.filename
+        else
+            --Error
+            return false
+        end
+
+    end
+end
+
+return MSTranslate
